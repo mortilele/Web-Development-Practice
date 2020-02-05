329b0198ff5b054c17f67ec4109cfcb91c992965
Alik <ali.mars.99@gmail.com>
2020-02-05 15:34:48 +0600

    chandeg js

--- ToDoList/main.js
+++ ToDoList/main.js
@@ -1,23 +1,31 @@
+ function trigger(e) {
+ 	if (e.keyCode == 13) {
+ 		add();
+    }
+ }
+
  function add() {
-            let data = document.getElementById("in").value;
-            let checkbox = document.createElement("input");
-            checkbox.setAttribute("type", "checkbox")
-            checkbox.addEventListener("click", function() { check(this)});
-            checkbox.setAttribute("class", "checker");
-            let label = document.createElement("label");
-            label.innerHTML = data;
-            let btn = document.createElement("button");
-            btn.addEventListener("click", function() { remove(this)});
-            btn.innerHTML = '-';
-            btn.setAttribute("class", "delete");
-            let block = document.createElement("div");
-            block.setAttribute("class", "item");
-            block.appendChild(checkbox);
-            block.appendChild(label);
-            block.appendChild(btn);
-            document.getElementById("items").appendChild(block);
-            document.getElementById("in").value = "";
-        }
+    let data = document.getElementById("in").value;
+	    if (data != "") {
+	    	let checkbox = document.createElement("input");
+	        checkbox.setAttribute("type", "checkbox")
+	        checkbox.addEventListener("click", function() { check(this)});
+	        checkbox.setAttribute("class", "checker");
+	        let label = document.createElement("label");
+	        label.innerHTML = data;
+	        let btn = document.createElement("button");
+	        btn.addEventListener("click", function() { remove(this)});
+	        btn.innerHTML = '-';
+	        btn.setAttribute("class", "delete");
+	        let block = document.createElement("div");
+	        block.setAttribute("class", "item");
+	        block.appendChild(checkbox);
+	        block.appendChild(label);
+	        block.appendChild(btn);
+	        document.getElementById("items").insertBefore(block, document.getElementById("items").childNodes[0]);
+	        document.getElementById("in").value = "";
+	    }
+}
 
 function remove(element) {
     document.getElementById("items").removeChild(element.parentNode);
