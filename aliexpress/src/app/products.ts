4be17bd701fe5ded5db36d3f88ef03f27c8d6747
Alik <ali.mars.99@gmail.com>
2020-02-12 02:15:14 +0600

    added aliexpress

---
+++ aliexpress/src/app/products.ts
@@ -0,0 +1,86 @@
+export const products = [
+  {
+    name: 'Laptop Gaming',
+    price: 600.0,
+    description: 'A large phone with one of the best screens',
+    rating: 5.0,
+    image: 'https://ae01.alicdn.com/kf/HTB1TOtuXSSD3KVjSZFKq6z10VXaC/MECHREVO-Z2-I5-I7-1050Ti-1060-144-15-6-Inch-Intel-8th-Laptop-Gaming-Laptop-Windows.jpg_220x220xz.jpg_.webp'
+  },
+  {
+    name: 'Hasee Notebook',
+    price: 3527.0,
+    description: 'A large phone with one of the best screens',
+    rating: 4.9,
+    image:'https://ae01.alicdn.com/kf/HTB1xI2QV9zqK1RjSZFpq6ykSXXav/Hasee-K670E-G6A5-Laptop-for-Gaming-Intel-9Gen-i5-9400-GTX1050-8G-RAM-512G-SSD-DOS.jpg_220x220xz.jpg_.webp'
+  },
+  {
+    name: 'Original Xiaomi',
+    price: 882.13,
+    description: 'A large phone with one of the best screens',
+    rating: 4.7,
+    image: 'https://ae01.alicdn.com/kf/Hbfdb709a5cfa4befa8c054e4a33f60a61/Original-Xiaomi-Notebook-Pro-15-6-inch-GTX-1050-Max-Q-4GB-GDDR5-Laptop-i7-8550U.jpg_220x220xz.jpg_.webp'
+  },
+  {
+    name: 'Hasee Laptop',
+    price: 753.12,
+    description: 'A large phone with one of the best screens',
+    rating: 5.0,
+    image: 'https://ae01.alicdn.com/kf/HTB1u1NzXNiH3KVjSZPfq6xBiVXaJ/MECHREVO-Z2-Electronic-Competitive-Screen-15-6-Inch-Intel-8th-Gaming-Laptop-Windows-10-Notebook-i7.jpg_220x220xz.jpg_.webp'
+  },
+  {
+    name: 'Xiaomi Gaming Laptop',
+    price: 2248.3,
+    description: 'A large phone with one of the best screens',
+    rating: 3.8,
+    image: 'https://ae01.alicdn.com/kf/H8293da656929462a9118fdc988f601e5X/Hasee-K670D-G4E6-Laptop-for-Gaming-Intel-9Gen-G5420-GTX1050-4G-8G-RAM-256G-SSD-15.jpg_220x220xz.jpg_.webp'
+  },
+  {
+    name: 'Huawei Honor MagicBook',
+    price: 400.3,
+    description: 'A large phone with one of the best screens',
+    rating: 4.5,
+    image: 'https://ae01.alicdn.com/kf/Hc5ebb68907d04032af88ac0244692cb2I/2019-Xiaomi-Mi-Gaming-Laptop-15-6-Intel-Core-i7-8750-Hexa-Core-4-1GHz-GTX.jpg_220x220xz.jpg_.webp'
+  },
+  {
+    name: 'Malbenben Notebook',
+    price: 711.55,
+    description: 'A large phone with one of the best screens',
+    rating: 4.7,
+    image: 'https://ae01.alicdn.com/kf/H14b2fe8fb17d4a5393a04a30f95e51688/Huawei-Honor-MagicBook-2019-font-b-Laptop-b-font-Notebook-Computer-14-inch-AMD-Ryzen-5.jpg_220x220xz.jpg_.webp'
+  },
+  {
+    name: 'Bben Laptop',
+    price: 600.0,
+    description: 'A large phone with one of the best screens',
+    rating: 5.0,
+    image: 'https://ae01.alicdn.com/kf/Ha7c52e2a41e84e8a8e32fca943c7b6a6L/2019-Xiaomi-Gaming-Laptop-15-6-inch-Windows-10-OS-Intel-Core-i7-9750H-Hexa-Core.jpg_220x220xz.jpg_.webp'
+  },
+  {
+    name: 'Lenovo Legion',
+    price: 790.5,
+    description: 'A large phone with one of the best screens',
+    rating: 4.7,
+    image: 'https://ae01.alicdn.com/kf/HTB1b1R1aGL7gK0jSZFBq6xZZpXai/MaiBenBen-X9-Plus-Intel-i7-9750H-GTX1050-3G-Graphics-Card-16G-RAM-512G-1TB-DOS-Win10.jpg_220x220xz.jpg_.webp'
+  },
+  {
+    name: 'Asus Nitro 5',
+    price: 1353.32,
+    description: 'A large phone with one of the best screens',
+    rating: 2,
+    image: 'https://ae01.alicdn.com/kf/HTB17WTkazDuK1RjSszdq6xGLpXat/Original-Xiaomi-Gaming-Laptop-15-6-inch-GTX-1060-Mi-Notebook-8th-Enhance-SSD-256G-1TB.jpg_220x220xz.jpg_.webp'
+  },
+  {
+    name: 'Macbook Pro',
+    price: 3019.83,
+    description: 'A large phone with one of the best screens',
+    rating: 0.0,
+    image: 'https://ae01.alicdn.com/kf/HTB17WTkazDuK1RjSszdq6xGLpXat/Original-Xiaomi-Gaming-Laptop-15-6-inch-GTX-1060-Mi-Notebook-8th-Enhance-SSD-256G-1TB.jpg_220x220xz.jpg_.webp'
+  },
+  {
+    name: 'Macbook Air',
+    price: 1019.5,
+    description: 'A large phone with one of the best screens',
+    rating: 4.3,
+    image: 'https://ae01.alicdn.com/kf/HTB17WTkazDuK1RjSszdq6xGLpXat/Original-Xiaomi-Gaming-Laptop-15-6-inch-GTX-1060-Mi-Notebook-8th-Enhance-SSD-256G-1TB.jpg_220x220xz.jpg_.webp'
+  },
+];
